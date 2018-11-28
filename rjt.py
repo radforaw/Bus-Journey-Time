b1dc6f2e83a99a5e32da95bd622296381fa5cf56
Ubuntu <ubuntu@ip-172-31-87-2.ec2.internal>
2018-11-28 09:09:53 +0000

    version

--- rjt.py
+++ rjt.py
@@ -1,3 +1,4 @@
+
 #! python3
 
 import requests
@@ -14,11 +15,9 @@ fileloc='/home/ubuntu/website/website/yourapp/static/'
 
 #fileloc=''
 
-quick={'Holloway Circus (inbound)':(43000305101,43000203501)}
+#quick={'Holloway-Circus-inbound':(43000305101,43000203501)}
 
-routes={'Holloway Circus (inbound)':(43000305101,43000203501),'Five Ways (inbound)':(43000301102,43000207102),'Spring Hill (inbound)':(43000286602,43000207202),'St Chads (inbound)':(43000276503,43000208501),'Dartmouth Circus (inbound)':(43000253404,43000207601),'Curzon Circus (inbound)':(43000241402,43002104401),'Garrison Circus (inbound)':(43000236202,43000211304),'Bordesley Circus (inbound)':(43000230203,43000202203),'Camp Hill Circus (inbound)':(43000220102,43000211304),'Bradford Street -  Markets (inbound)':(43000218202,43000202301),'Belgrave Interchange (inbound)':(43000343202,43000203902)}
-
-back={'harborne (outbound)':(43000205601,43000305201),'Five Ways (outbound)':(43000205601,43003003501),'Spring Hill (outbound)':(43000205601,43002870101),'St Chads (outbound)':(43000207205,43000276301),'Dartmouth (outbound)':(43000252102,43000253601),'Curzon (outbound)':(43002104402,43000241502),'Garrison (outbound)':(43000212601,43000236402),'Camp Hill (outbound)':(43000211501,43000220201),'Bordesley (outbound)':(43000211502,43000230302),'Moseley (outbound)':(43002103506,43000218101),'Pershore (outbound)':(43000213202,43000343201)}
+routes={'Holloway-Circus-inbound':(43000305101,43000203501),'Five-Ways-inbound':(43000301102,43000207102),'Spring-Hill-inbound':(43000286602,43000207202),'St-Chads-inbound':(43000276503,43000208501),'Dartmouth-Circus-inbound':(43000253404,43000207601),'Curzon-Circus-inbound':(43000241402,43002104401),'Garrison-Circus-inbound':(43000236202,43000211304),'Bordesley-Circus-inbound':(43000230203,43000202203),'Camp-Hill-Circus-inbound':(43000220102,43000211304),'Bradford-Street----Markets-inbound':(43000218202,43000202301),'Belgrave-Interchange-inbound':(43000343202,43000203902),'harborne-outbound':(43000205601,43000305201),'Five-Ways-outbound':(43000205601,43003003501),'Spring-Hill-outbound':(43000205601,43002870101),'St-Chads-outbound':(43000207205,43000276301),'Dartmouth-outbound':(43000252102,43000253601),'Curzon-outbound':(43002104402,43000241502),'Garrison-outbound':(43000212601,43000236402),'Camp-Hill-outbound':(43000211501,43000220201),'Bordesley-outbound':(43000211502,43000230302),'Moseley-outbound':(43002103506,43000218101),'Pershore-outbound':(43000213202,43000343201)}
 
 thresh=1.2
 
@@ -33,26 +32,9 @@ def getdata(route,day=0):
 	tf=False
 	if day==0:
 		tf=True
-	#print ('aaa ',dy,tf)
 	dys=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-	#print (dys[dy.weekday()])
-	tm=dy.strftime('%Y-%m-%d')
-	#print (tm)
-	
+	tm=dy.strftime('%Y-%m-%d')	
 	url='https://realjourneytime.azurewebsites.net/index.php?method=Journeys&fromCode='+str(route[0])+'&toCode='+str(route[1])+'&dateString='+tm
-	#print (url)
-	'''
-	a= False
-	tries=5
-	while a==False and tries>0:
-		try:
-			a=requests.get(url,timeout=20)
-		except:
-			print('retry')
-			tries-=1
-	if a==False:
-		return False
-	'''
 	l=dt.rcache()
 	try:
 		r=json.loads(l.get(url,nc=tf))
@@ -81,14 +63,14 @@ def totalday(res,ct):
 		return 0
 	retval=[(a[2]/a[1])>thresh for a in res]
 	s=sorted([a[2]/a[1]for a in res])[int(len(res)*.85)]
-	j=[[datetime.datetime.strptime(a[3],'%Y-%m-%dT%H:%M:%S').time(),(a[2]/a[1])>thresh] for a in res]
+	j=[[datetime.datetime.strptime(a[3],'%Y-%m-%dT%H:%M:%SZ').time(),(a[2]/a[1])>thresh] for a in res]
 	k=[a[0].hour+a[0].minute/60.0 for a in j if a[1]]
 	m=[a[0].hour+a[0].minute/60.0 for a in j if not a[1]]
-	size=[(a[2]/a[1])*64 for a in res if (a[2]/a[1])>thresh]
+	size=[(((a[2]/a[1])**2)*4 )+4 for a in res if (a[2]/a[1])>thresh]
 	#print ([a[1] for a in res])
 
 	plt.scatter([a for a in k],[ct for n in range(len(k))],c='red',marker='d',s=size,edgecolors='face')
-	plt.scatter([a for a in m],[ct for n in range(len(m))],c='blue',edgecolors='face')
+	plt.scatter([a for a in m],[ct for n in range(len(m))],c='blue',s=4,edgecolors='face')
 	return (float(sum(retval))/len(res))*100,s
 
 def drawgraph(res):
@@ -105,7 +87,7 @@ def drawgraph(res):
 def cumulativedelayminutes(res):
 	if len(res)==0:
 		return 0
-	print (datetime.datetime.strptime(res[0][3],'%Y-%m-%dT%H:%M:%S').date())
+	print (datetime.datetime.strptime(res[0][3],'%Y-%m-%dT%H:%M:%SZ').date())
 	print (sum([a[2]-a[1] if a[2]-a[1]>=0 else 0 for a in res])/60)
 	return sum([a[2]-a[1] if a[2]-a[1]>=0 else 0 for a in res])/60
 	
@@ -123,27 +105,21 @@ if __name__=='__main__':
 	import time
 	while True:
 		plt.style.use('ggplot')
-		#print ("is it fucked?")
-
-		#a=getdata(back['const hill']
-		#quick=routes
-		ct=1
-		g2=[]
+		quick=routes
 		for n in quick:
+			ct=1
 			for j in range(28):#[30,23,16,9,2]:
 				a=getdata(quick[n],day=j)
 				if a==False:
 					continue
 				print (n,quickyaynay(a),totalday(a,ct))#datetime.datetime.strptime(a[-1][3],'%Y-%m-%dT%H:%M:%S'),a[-1][3])
 				ct+=1
-			ct=1
-			plt.title(n+"\nUpdated "+str(datetime.datetime.now()))
+			plt.title(n+"\nUpdated "+str(datetime.datetime.now()),fontsize=10)
 			x1,x2,y1,y2 = plt.axis()
 			plt.axis((5,24,y1,y2))
-			plt.xlabel('Hour Starting')
-			plt.ylabel('Days')
-			plt.savefig(fileloc+'bus.png')
-			#plt.show()
+			plt.xlabel('Hour Starting',fontsize=8)
+			plt.ylabel('Days',fontsize=8)
+			plt.savefig(fileloc+n+'bus.png',dpi=150)
 			plt.close()
 			
 			wks=8
@@ -151,29 +127,22 @@ if __name__=='__main__':
 			g2=[]
 			for a in thing:
 				l= ((datetime.datetime.now().date()-a[2]).days)
-				print (l)
 				if l>0:
-					#try:
 					g2.append([a[1],cumulativedelayminutes(getdata(quick[n],day=l))])
-					#except:
-						#g2.append([a[1],0])
 				else:
-						g2.append([a[1],0])
+					g2.append([a[1],0])
 				print (g2[-1])
 			col=[z for z in ['red','green','blue','purple','black','orange','grey']for zz in range(wks+1)]
-			print (col)
-				
 			plt.bar(range(len(g2)),[b[1] for b in g2],color=col)
 			start, end = plt.gca().get_xlim()
-			print (start,end)
-			plt.xticks(range(int(len(g2))),[a[0] for a in g2],rotation=45)
+			plt.xticks(range(int(len(g2))),[a[0] for a in g2],rotation=90,fontsize=6)
 			plt.title(n+" Bus Delays\nUpdated "+str(datetime.datetime.now()))
 			plt.xlabel('Day')
 			plt.ylabel('Delay minutes')
-			plt.savefig(fileloc+"busdelay.png")
+			plt.savefig(fileloc+n+"busdelay.png")
 			plt.close()
-			#sys.exit(0)
-		time.sleep(900)
+			time.sleep(60)
+
 
 # how many times 10% above threshold
 # maximum delay
